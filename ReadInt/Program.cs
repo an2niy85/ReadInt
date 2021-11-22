a4ec00fd47ca9c61a3398341e8af516d9ef73098
Антон Красноперов <an2niy85@mail.ru>
2021-11-22 18:30:21 +0400

    Добавьте файлы проекта.

---
+++ ReadInt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            number = ReadInt();
+            Console.WriteLine(number);
+            Console.ReadKey();
+        }
+
+        static int ReadInt()
+        {
+            int number;
+            bool True;
+
+            do
+            {
+                Console.Write("Enter number between 0 t0 10: ");
+                True = int.TryParse(Console.ReadLine(), out number);
+
+            } while (!True);
+            return number;
+        }
+    }
+}
